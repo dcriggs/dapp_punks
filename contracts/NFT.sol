af34c86a2778b8b907407bf388fdef63c1dd602c
dcriggs <dawsoncriggs@gmail.com>
2024-07-22 10:55:33 -0600

    Mint NFT functions and tests

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,10 +5,15 @@ import "./ERC721Enumerable.sol";
 import "./Ownable.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
     uint256 public cost;
     uint256 public maxSupply;
     uint256 public allowMintingOn;
     string public baseURI;
+    string public baseExtension = ".json";
+
+    event Mint(uint256 amount, address minter);
 
     constructor(
         string memory _name,
@@ -22,5 +27,35 @@ contract NFT is ERC721Enumerable, Ownable {
         maxSupply = _maxSupply;
         allowMintingOn = _allowMintingOn;
         baseURI = _baseURI;
+    }
+
+    function mint(uint256 _mintAmount) public payable {
+        require(block.timestamp >= allowMintingOn);
+
+        require(_mintAmount > 0);
+
+        require(msg.value >= cost * _mintAmount);
+
+        uint256 supply = totalSupply();
+
+        require(supply + _mintAmount <= maxSupply);
+
+        for (uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + 1);
+        }
+
+        emit Mint(_mintAmount, msg.sender);
+    }
+
+    function tokenURI(
+        uint256 _tokenId
+    ) public view virtual override returns (string memory) {
+        require(_exists(_tokenId), "token does not exist");
+
+        return (
+            string(
+                abi.encodePacked(baseURI, _tokenId.toString(), baseExtension)
+            )
+        );
     }
 }
